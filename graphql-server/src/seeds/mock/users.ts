221413a5715c6b155a52414581dac2085e5cfe21
Daniel-Kahalani <kahalan179@outlook.com>
2022-03-20 10:00:23 +0200

    change seed password to unhash password

--- graphql-server/src/seeds/mock/users.ts
+++ graphql-server/src/seeds/mock/users.ts
@@ -1,6 +1,6 @@
 export default [
-  {
-    username: "daniel",
-    password: "$2a$12$/4IIqWIHWimP9F0pNfULIe5v0L51ZkgNF7PiK9PPncZQFCUvWECpq",
-  },
+	{
+		username: 'daniel',
+		password: '!1234567',
+	},
 ];
