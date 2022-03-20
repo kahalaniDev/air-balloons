95185b62a8c1b8893831d4a833e1200818a50981
Daniel-Kahalani <kahalan179@outlook.com>
2022-03-20 17:26:15 +0200

    change to live rest server

--- rest-server/DTO/UserDTO.cs
+++ rest-server/DTO/UserDTO.cs
@@ -13,28 +13,30 @@ namespace rest_server.DTO
     public class UserDTO
     {
 
-        public UserDTO(User user,string jwtSecret){
+        public UserDTO(User user, string jwtSecret)
+        {
             this.Username = user.Username;
-            this.Token = createToken(user.Username,jwtSecret);
+            this.Token = createToken(user.Username, jwtSecret);
         }
 
-        private string createToken(string username,string jwtSecret)
+        private string createToken(string username, string jwtSecret)
         {
-			var tokenHandler = new JwtSecurityTokenHandler();
-			var tokenKey = Encoding.ASCII.GetBytes(jwtSecret);
-			var tokenDescriptor = new SecurityTokenDescriptor
-			{
-				Subject = new ClaimsIdentity(new[] { new Claim("username", username) }),
-				Expires = DateTime.UtcNow.AddDays(7),
-				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
-			};
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.ASCII.GetBytes(jwtSecret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("username", username) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+            };
 
-			var token = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.CreateToken(tokenDescriptor);
 
-			return tokenHandler.WriteToken(token);        }
+            return tokenHandler.WriteToken(token);
+        }
 
-        public string Username { get; set; } 
-        public string Token { get; set; } 
+        public string Username { get; set; }
+        public string Token { get; set; }
 
     }
 }
