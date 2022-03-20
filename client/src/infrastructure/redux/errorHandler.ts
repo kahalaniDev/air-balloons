2894ee6a0f475a1a5905820796d78d88d5d2d7c8
Daniel-Kahalani <kahalan179@outlook.com>
2022-03-20 12:54:55 +0200

    client add serverErrorBuilder in axiosErrorBuilder

--- client/src/infrastructure/redux/errorHandler.ts
+++ client/src/infrastructure/redux/errorHandler.ts
@@ -1,6 +1,5 @@
 import { ApolloError, isApolloError } from '@apollo/client';
 import axios, { AxiosError } from 'axios';
-import { GraphQLError } from 'graphql';
 import { IAppError } from '../../models/interfaces';
 
 const GLOBAL_STATUS_CODE = 500;
@@ -11,34 +10,32 @@ export const errorHandler = (err: unknown) => {
 	if (axios.isAxiosError(err)) return axiosErrorBuilder(err);
 	if (err instanceof Error && isApolloError(err))
 		return apolloErrorBuilder(err);
-	return {
-		statusCode: 500,
-		message: INTERNAL_ERR_MSG,
-	};
+	return { statusCode: GLOBAL_STATUS_CODE, message: INTERNAL_ERR_MSG };
 };
 
 const axiosErrorBuilder = (axiosError: AxiosError) => {
-	const statusCode = axiosError.response!.status;
-	switch (statusCode) {
-		case 401:
-		case 409:
-		default:
-			return { statusCode, message: INTERNAL_ERR_MSG };
-	}
+	return serverErrorBuilder(
+		axiosError.response!.data,
+		axiosError.response!.status
+	);
 };
 
 const apolloErrorBuilder = (apolloError: ApolloError): IAppError => {
 	if (apolloError.networkError)
 		return { statusCode: GLOBAL_STATUS_CODE, message: NEWTWORK_ERR_MSG };
-	return graphqlErrorBuilder(apolloError.graphQLErrors[0]);
+
+	const graphQLError = apolloError.graphQLErrors[0];
+	return serverErrorBuilder(
+		graphQLError.message,
+		graphQLError.extensions.codeNum as number
+	);
 };
 
-const graphqlErrorBuilder = (graphQLError: GraphQLError): IAppError => {
-	const statusCode = graphQLError.extensions.codeNum as number;
-	switch (graphQLError.extensions.codeNum) {
+const serverErrorBuilder = (message: string, statusCode: number): IAppError => {
+	switch (statusCode) {
 		case 401:
 		case 409:
-			return { statusCode, message: graphQLError.message };
+			return { statusCode, message };
 		default:
 			return { statusCode, message: INTERNAL_ERR_MSG };
 	}
