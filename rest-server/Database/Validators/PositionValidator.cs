f0112b7f79d803dee865ca69b3362fcb2c2ba49a
Daniel-Kahalani <kahalan179@outlook.com>
2022-03-20 14:22:43 +0200

    rest-server change logitude range values to -180 - 180

--- rest-server/Database/Validators/PositionValidator.cs
+++ rest-server/Database/Validators/PositionValidator.cs
@@ -9,13 +9,13 @@ namespace rest_server.Database.Validators
 {
     public class PositionValidator : AbstractValidator<Position>
     {
-      
-		public PositionValidator()
-		{
+
+        public PositionValidator()
+        {
             RuleFor(position => position.Id).Null();
-            RuleFor(position => position.Longitude).NotNull().GreaterThanOrEqualTo(-90).LessThanOrEqualTo(90);
+            RuleFor(position => position.Longitude).NotNull().GreaterThanOrEqualTo(-180).LessThanOrEqualTo(180);
             RuleFor(position => position.Latitude).NotNull().GreaterThanOrEqualTo(-90).LessThanOrEqualTo(90);
             RuleFor(position => position.Altitude).NotNull().GreaterThanOrEqualTo(0);
-		}
+        }
     }
 }
