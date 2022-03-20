9e33051fd1910a1622e2e000cb610f6c8c718b8a
Daniel-Kahalani <kahalan179@outlook.com>
2022-03-20 12:41:44 +0200

    client change apolloErrorBuilder to support errorNum

--- client/src/infrastructure/redux/errorHandler.ts
+++ client/src/infrastructure/redux/errorHandler.ts
@@ -1,65 +1,63 @@
-import { ApolloError, isApolloError } from "@apollo/client";
-import axios, { AxiosError } from "axios";
-import { GraphQLError } from "graphql";
-import { IAppError } from "../../models/interfaces";
+import { ApolloError, isApolloError } from '@apollo/client';
+import axios, { AxiosError } from 'axios';
+import { GraphQLError } from 'graphql';
+import { IAppError } from '../../models/interfaces';
 
 const GLOBAL_STATUS_CODE = 500;
-const INTERNAL_ERR_MSG = "Internal server problem, please try again";
-const NEWTWORK_ERR_MSG = "Unable to access the server, please try again";
+const INTERNAL_ERR_MSG = 'Internal server problem, please try again';
+const NEWTWORK_ERR_MSG = 'Unable to access the server, please try again';
 
 export const errorHandler = (err: unknown) => {
-  if (axios.isAxiosError(err)) return axiosErrorBuilder(err);
-  if (err instanceof Error && isApolloError(err))
-    return apolloErrorBuilder(err);
-  return {
-    statusCode: 500,
-    message: INTERNAL_ERR_MSG,
-  };
+	if (axios.isAxiosError(err)) return axiosErrorBuilder(err);
+	if (err instanceof Error && isApolloError(err))
+		return apolloErrorBuilder(err);
+	return {
+		statusCode: 500,
+		message: INTERNAL_ERR_MSG,
+	};
 };
 
 const axiosErrorBuilder = (axiosError: AxiosError) => {
-  console.log(axiosError);
-  const statusText = axiosError.response?.statusText
-    ? axiosError.response!.statusText
-    : "";
-  const statusCode = axiosError.response!.status;
-  switch (statusText) {
-    case "Not Found":
-      return { statusCode, message: axiosError.response!.data };
-    case "Bad Request":
-      return { statusCode, message: axiosError.response!.data };
-    case "Conflict":
-      return { statusCode, message: axiosError.response!.data };
-    case "Unauthorized":
-      return { statusCode, message: axiosError.response!.data };
-    default:
-      return { statusCode, message: INTERNAL_ERR_MSG };
-  }
-  // return {
-  //   statusCode: axiosError.response?.status
-  //     ? axiosError.response?.data?.statusCode
-  //     : axiosError.code,
-  //   message: axiosError.response?.data?.message
-  //     ? axiosError.response?.data?.message
-  //     : INTERNAL_ERR_MSG,
-  // };
+	console.log(axiosError);
+	const statusText = axiosError.response?.statusText
+		? axiosError.response!.statusText
+		: '';
+	const statusCode = axiosError.response!.status;
+	switch (statusText) {
+		case 'Not Found':
+			return { statusCode, message: axiosError.response!.data };
+		case 'Bad Request':
+			return { statusCode, message: axiosError.response!.data };
+		case 'Conflict':
+			return { statusCode, message: axiosError.response!.data };
+		case 'Unauthorized':
+			return { statusCode, message: axiosError.response!.data };
+		default:
+			return { statusCode, message: INTERNAL_ERR_MSG };
+	}
+	// return {
+	//   statusCode: axiosError.response?.status
+	//     ? axiosError.response?.data?.statusCode
+	//     : axiosError.code,
+	//   message: axiosError.response?.data?.message
+	//     ? axiosError.response?.data?.message
+	//     : INTERNAL_ERR_MSG,
+	// };
 };
 
 const apolloErrorBuilder = (apolloError: ApolloError): IAppError => {
-  if (apolloError.networkError)
-    return { statusCode: GLOBAL_STATUS_CODE, message: NEWTWORK_ERR_MSG };
-  if (apolloError.graphQLErrors)
-    return graphqlErrorBuilder(apolloError.graphQLErrors[0]);
-  return { statusCode: GLOBAL_STATUS_CODE, message: INTERNAL_ERR_MSG };
+	if (apolloError.networkError)
+		return { statusCode: GLOBAL_STATUS_CODE, message: NEWTWORK_ERR_MSG };
+	return graphqlErrorBuilder(apolloError.graphQLErrors[0]);
 };
 
 const graphqlErrorBuilder = (graphQLError: GraphQLError): IAppError => {
-  const statusCode = graphQLError.extensions.codeNum as number;
-  switch (graphQLError.extensions.code) {
-    case "UNAUTHENTICATED":
-    case "BAD_USER_INPUT":
-      return { statusCode, message: graphQLError.message };
-    default:
-      return { statusCode, message: INTERNAL_ERR_MSG };
-  }
+	const statusCode = graphQLError.extensions.codeNum as number;
+	switch (graphQLError.extensions.codeNum) {
+		case 401:
+		case 409:
+			return { statusCode, message: graphQLError.message };
+		default:
+			return { statusCode, message: INTERNAL_ERR_MSG };
+	}
 };
