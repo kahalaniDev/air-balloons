8973e4e879da38546d9e925b67929adc17ddf19d
Daniel-Kahalani <kahalan179@outlook.com>
2022-03-20 17:01:13 +0200

    client add disable button to BalloonForm until any  value change

--- client/src/features/balloons/components/BalloonForm.tsx
+++ client/src/features/balloons/components/BalloonForm.tsx
@@ -1,188 +1,194 @@
-import React, { useState, useEffect } from "react";
-import { useApolloClient } from "@apollo/client";
-import useAppDispatch from "../../../hooks/useAppDispatch";
-import { addBalloon, getBalloons, resetError } from "../slices/balloonsSlice";
-import { logout } from "../../account/slices/accountSlice";
-import { addBalloonGraphql } from "../api/addBalloon/addBalloonGraphql";
-import { addBalloonRest } from "../api/addBalloon/addBalloonRest";
-import { getBalloonsGraphql } from "../api/getBalloons/getBalloonsGraphql";
-import { getBalloonsRest } from "../api/getBalloons/getBalloonsRest";
-import useInput from "../../../hooks/useInput";
-import useSelectInput from "../../../hooks/useSelectInput";
-import { IAppError } from "../../../models/interfaces";
-import { IBalloon, IPosition } from "../models/interfaces";
-import Position from "../models/Position";
-import Balloon from "../models/Balloon";
-import { BalloonColor, BalloonType } from "../models/enums";
-import { ACTIVE_SERVER, SERVER_TYPE } from "../../../infrastructure/config";
+import React, { useState, useEffect } from 'react';
+import { useApolloClient } from '@apollo/client';
+import useAppDispatch from '../../../hooks/useAppDispatch';
+import { addBalloon, getBalloons, resetError } from '../slices/balloonsSlice';
+import { logout } from '../../account/slices/accountSlice';
+import { addBalloonGraphql } from '../api/addBalloon/addBalloonGraphql';
+import { addBalloonRest } from '../api/addBalloon/addBalloonRest';
+import { getBalloonsGraphql } from '../api/getBalloons/getBalloonsGraphql';
+import { getBalloonsRest } from '../api/getBalloons/getBalloonsRest';
+import useInput from '../../../hooks/useInput';
+import useSelectInput from '../../../hooks/useSelectInput';
+import { IAppError } from '../../../models/interfaces';
+import { IBalloon, IPosition } from '../models/interfaces';
+import Position from '../models/Position';
+import Balloon from '../models/Balloon';
+import { BalloonColor, BalloonType } from '../models/enums';
+import { ACTIVE_SERVER, SERVER_TYPE } from '../../../infrastructure/config';
 import {
-  validateColor,
-  validateDescription,
-  validateLatitude,
-  validateLongitude,
-  validateName,
-  validateType,
-} from "../utils/validators";
-import BalloonInfoInputs from "./BalloonInfoInputs";
-import BalloonPositionInputs from "./BalloonPositionInputs";
-import FormButton from "../../../components/form/FormButton";
-import FormError from "../../../components/form/FormError";
-import { Grid } from "@mui/material";
+	validateColor,
+	validateDescription,
+	validateLatitude,
+	validateLongitude,
+	validateName,
+	validateType,
+} from '../utils/validators';
+import BalloonInfoInputs from './BalloonInfoInputs';
+import BalloonPositionInputs from './BalloonPositionInputs';
+import FormButton from '../../../components/form/FormButton';
+import FormError from '../../../components/form/FormError';
+import { Grid } from '@mui/material';
 
 type Props = {
-  onSubmitFinish?: () => void;
-  balloon: IBalloon | undefined;
-  balloonPosition: IPosition | undefined;
-  error: IAppError | undefined;
-  loading: boolean;
+	onSubmitFinish?: () => void;
+	balloon: IBalloon | undefined;
+	balloonPosition: IPosition | undefined;
+	error: IAppError | undefined;
+	loading: boolean;
 };
 
 const formErrorCodes = [404];
 const ignoreErrorCodes = [409, 401];
 
 const BalloonForm: React.FC<Props> = ({
-  balloon,
-  balloonPosition,
-  onSubmitFinish,
-  error,
-  loading,
+	balloon,
+	balloonPosition,
+	onSubmitFinish,
+	error,
+	loading,
 }) => {
-  const dispatch = useAppDispatch();
-  const [popupOpen, setPopupOpen] = useState(false);
-  const client = useApolloClient();
+	const dispatch = useAppDispatch();
+	const [popupOpen, setPopupOpen] = useState(false);
+	const client = useApolloClient();
 
-  const nameUseInput = useInput(balloon ? balloon.name : "", validateName);
-  const [name, , , isNameValid, , , setNameError] = nameUseInput;
+	const isValuesChangedUseState = useState(false);
+	const [isValuesChanged] = isValuesChangedUseState;
 
-  const descriptionUseInput = useInput(
-    balloon ? balloon.description : "",
-    validateDescription
-  );
-  const [description, , , isDescriptionValid, ,] = descriptionUseInput;
+	const nameUseInput = useInput(balloon ? balloon.name : '', validateName);
+	const [name, , , isNameValid, , , setNameError] = nameUseInput;
 
-  const latitudeUseInput = useInput(
-    balloonPosition ? balloonPosition.latitude.toFixed(4) : "",
-    validateLatitude
-  );
-  const [latitude, , , isLatitudeValid, ,] = latitudeUseInput;
+	const descriptionUseInput = useInput(
+		balloon ? balloon.description : '',
+		validateDescription
+	);
+	const [description, , , isDescriptionValid, ,] = descriptionUseInput;
 
-  const longitudeUseInput = useInput(
-    balloonPosition ? balloonPosition.longitude.toFixed(4) : "",
-    validateLongitude
-  );
-  const [longitude, , , isLongitudeValid, ,] = longitudeUseInput;
+	const latitudeUseInput = useInput(
+		balloonPosition ? balloonPosition.latitude.toFixed(4) : '',
+		validateLatitude
+	);
+	const [latitude, , , isLatitudeValid, ,] = latitudeUseInput;
 
-  const typeUseSelectInput = useSelectInput(
-    balloon ? balloon.type : "",
-    validateType
-  );
-  const [type, , , isTypeValid] = typeUseSelectInput;
+	const longitudeUseInput = useInput(
+		balloonPosition ? balloonPosition.longitude.toFixed(4) : '',
+		validateLongitude
+	);
+	const [longitude, , , isLongitudeValid, ,] = longitudeUseInput;
 
-  const colorUseSelectInput = useSelectInput(
-    balloon ? balloon.color : "",
-    validateColor
-  );
-  const [color, , , isColorValid] = colorUseSelectInput;
+	const typeUseSelectInput = useSelectInput(
+		balloon ? balloon.type : '',
+		validateType
+	);
+	const [type, , , isTypeValid] = typeUseSelectInput;
 
-  const isFormValidate = () => {
-    return (
-      isNameValid(false) &&
-      isDescriptionValid(false) &&
-      isTypeValid(false) &&
-      isColorValid(false) &&
-      isLatitudeValid(false) &&
-      isLongitudeValid(false)
-    );
-  };
+	const colorUseSelectInput = useSelectInput(
+		balloon ? balloon.color : '',
+		validateColor
+	);
+	const [color, , , isColorValid] = colorUseSelectInput;
 
-  const handleSubmit = async (evt: React.FormEvent) => {
-    const balloonId = (balloon ? balloon.id : "") as string;
-    const balloonPos = new Position(
-      parseFloat(longitude),
-      parseFloat(latitude)
-    );
-    const newBalloon = new Balloon(
-      balloonId,
-      name,
-      description,
-      type as BalloonType,
-      color as BalloonColor,
-      balloonPos
-    );
-    const resultAction = await dispatch(
-      addBalloon({
-        balloon: newBalloon,
-        addBallonRequest:
-          ACTIVE_SERVER === SERVER_TYPE.APOLLO
-            ? (balloon: IBalloon) => addBalloonGraphql(client, balloon)
-            : addBalloonRest,
-      })
-    );
-    if (addBalloon.fulfilled.match(resultAction)) {
-      onSubmitFinish && onSubmitFinish();
-      dispatch(
-        getBalloons(
-          ACTIVE_SERVER === SERVER_TYPE.APOLLO
-            ? () => getBalloonsGraphql(client)
-            : getBalloonsRest
-        )
-      );
-    } else if (
-      !formErrorCodes.includes(resultAction.payload!.statusCode) &&
-      !ignoreErrorCodes.includes(resultAction.payload!.statusCode)
-    )
-      setPopupOpen(true);
-    else if (resultAction.payload!.statusCode === 409) {
-      setNameError(resultAction.payload!.message);
-    } else if (resultAction.payload!.statusCode === 401) {
-      dispatch(logout());
-    }
-  };
+	const isFormValidate = () => {
+		return (
+			isValuesChanged &&
+			isNameValid(false) &&
+			isDescriptionValid(false) &&
+			isTypeValid(false) &&
+			isColorValid(false) &&
+			isLatitudeValid(false) &&
+			isLongitudeValid(false)
+		);
+	};
 
-  useEffect(() => {
-    return () => {
-      dispatch(resetError());
-    };
-  }, [dispatch]);
+	const handleSubmit = async (evt: React.FormEvent) => {
+		const balloonId = (balloon ? balloon.id : '') as string;
+		const balloonPos = new Position(
+			parseFloat(longitude),
+			parseFloat(latitude)
+		);
+		const newBalloon = new Balloon(
+			balloonId,
+			name,
+			description,
+			type as BalloonType,
+			color as BalloonColor,
+			balloonPos
+		);
+		const resultAction = await dispatch(
+			addBalloon({
+				balloon: newBalloon,
+				addBallonRequest:
+					ACTIVE_SERVER === SERVER_TYPE.APOLLO
+						? (balloon: IBalloon) =>
+								addBalloonGraphql(client, balloon)
+						: addBalloonRest,
+			})
+		);
+		if (addBalloon.fulfilled.match(resultAction)) {
+			onSubmitFinish && onSubmitFinish();
+			dispatch(
+				getBalloons(
+					ACTIVE_SERVER === SERVER_TYPE.APOLLO
+						? () => getBalloonsGraphql(client)
+						: getBalloonsRest
+				)
+			);
+		} else if (
+			!formErrorCodes.includes(resultAction.payload!.statusCode) &&
+			!ignoreErrorCodes.includes(resultAction.payload!.statusCode)
+		)
+			setPopupOpen(true);
+		else if (resultAction.payload!.statusCode === 409) {
+			setNameError(resultAction.payload!.message);
+		} else if (resultAction.payload!.statusCode === 401) {
+			dispatch(logout());
+		}
+	};
 
-  return (
-    <Grid
-      spacing={2}
-      container
-      component="form"
-      noValidate
-      sx={{ mt: 1 }}
-      data-testid="form"
-      maxWidth={450}
-    >
-      <BalloonInfoInputs
-        nameUseInput={nameUseInput}
-        descriptionUseInput={descriptionUseInput}
-        typeUseSelectInput={typeUseSelectInput}
-        colorUseSelectInput={colorUseSelectInput}
-        disabled={loading}
-      />
-      <BalloonPositionInputs
-        latitudeUseInput={latitudeUseInput}
-        longitudeUseInput={longitudeUseInput}
-        disabled={loading}
-      />
-      <Grid item xs={12}>
-        <FormButton
-          disabled={!isFormValidate()}
-          handleSubmit={handleSubmit}
-          title="Submit"
-          loading={loading}
-        />
-        <FormError
-          popupOpen={popupOpen}
-          closePopup={() => setPopupOpen(false)}
-          error={error}
-          formErrorCodes={formErrorCodes}
-        />
-      </Grid>
-    </Grid>
-  );
+	useEffect(() => {
+		return () => {
+			dispatch(resetError());
+		};
+	}, [dispatch]);
+
+	return (
+		<Grid
+			spacing={2}
+			container
+			component='form'
+			noValidate
+			sx={{ mt: 1 }}
+			data-testid='form'
+			maxWidth={450}>
+			<BalloonInfoInputs
+				nameUseInput={nameUseInput}
+				descriptionUseInput={descriptionUseInput}
+				typeUseSelectInput={typeUseSelectInput}
+				colorUseSelectInput={colorUseSelectInput}
+				disabled={loading}
+				isValuesChangedUseState={isValuesChangedUseState}
+			/>
+			<BalloonPositionInputs
+				latitudeUseInput={latitudeUseInput}
+				longitudeUseInput={longitudeUseInput}
+				disabled={loading}
+				isValuesChangedUseState={isValuesChangedUseState}
+			/>
+			<Grid item xs={12}>
+				<FormButton
+					disabled={!isFormValidate()}
+					handleSubmit={handleSubmit}
+					title='Submit'
+					loading={loading}
+				/>
+				<FormError
+					popupOpen={popupOpen}
+					closePopup={() => setPopupOpen(false)}
+					error={error}
+					formErrorCodes={formErrorCodes}
+				/>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default BalloonForm;
--- client/src/features/balloons/components/BalloonPositionInputs.tsx
+++ client/src/features/balloons/components/BalloonPositionInputs.tsx
@@ -1,56 +1,80 @@
-import React from "react";
-import { UseInputValue } from "../../../hooks/useInput";
-import { Grid, TextField } from "@mui/material";
+import React from 'react';
+import { UseInputValue } from '../../../hooks/useInput';
+import { Grid, TextField } from '@mui/material';
 
 type Props = {
-  disabled: boolean;
-  latitudeUseInput: UseInputValue;
-  longitudeUseInput: UseInputValue;
+	disabled: boolean;
+	latitudeUseInput: UseInputValue;
+	longitudeUseInput: UseInputValue;
+	isValuesChangedUseState: [
+		boolean,
+		React.Dispatch<React.SetStateAction<boolean>>
+	];
 };
 
 const BalloonPositionInputs: React.FC<Props> = ({
-  disabled,
-  latitudeUseInput,
-  longitudeUseInput,
+	disabled,
+	latitudeUseInput,
+	longitudeUseInput,
+	isValuesChangedUseState,
 }) => {
-  const [latitude, handleLatitudeChange, handleLatitudeBlur, , latitudeError] =
-    latitudeUseInput;
-  const [
-    longitude,
-    handleLongitudeChange,
-    handleLongitudeBlur,
-    ,
-    longitudeError,
-  ] = longitudeUseInput;
+	const [isValuesChanged, setIsValuesChanged] = isValuesChangedUseState;
 
-  return (
-    <>
-      <Grid item xs={6}>
-        <TextField
-          margin="normal"
-          disabled={disabled}
-          value={latitude}
-          onChange={handleLatitudeChange}
-          onBlur={handleLatitudeBlur}
-          error={latitudeError ? true : false}
-          helperText={latitudeError}
-          label="Latitude"
-        />
-      </Grid>
-      <Grid item xs={6}>
-        <TextField
-          margin="normal"
-          disabled={disabled}
-          value={longitude}
-          onChange={handleLongitudeChange}
-          onBlur={handleLongitudeBlur}
-          error={longitudeError ? true : false}
-          helperText={longitudeError}
-          label="Longitude"
-        />
-      </Grid>
-    </>
-  );
+	const [
+		latitude,
+		handleLatitudeChange,
+		handleLatitudeBlur,
+		,
+		latitudeError,
+	] = latitudeUseInput;
+	const [
+		longitude,
+		handleLongitudeChange,
+		handleLongitudeBlur,
+		,
+		longitudeError,
+	] = longitudeUseInput;
+
+	const handleInputChange = (
+		evt: React.ChangeEvent<HTMLInputElement>,
+		handleChange: (evt: React.ChangeEvent<HTMLInputElement>) => void
+	) => {
+		if (!isValuesChanged) setIsValuesChanged(true);
+		handleChange(evt);
+	};
+
+	return (
+		<>
+			<Grid item xs={6}>
+				<TextField
+					margin='normal'
+					disabled={disabled}
+					value={latitude}
+					onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+						handleInputChange(evt, handleLatitudeChange)
+					}
+					onBlur={handleLatitudeBlur}
+					error={latitudeError ? true : false}
+					helperText={latitudeError}
+					label='Latitude'
+				/>
+			</Grid>
+			<Grid item xs={6}>
+				<TextField
+					margin='normal'
+					disabled={disabled}
+					value={longitude}
+					onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+						handleInputChange(evt, handleLongitudeChange)
+					}
+					onBlur={handleLongitudeBlur}
+					error={longitudeError ? true : false}
+					helperText={longitudeError}
+					label='Longitude'
+				/>
+			</Grid>
+		</>
+	);
 };
 
 export default BalloonPositionInputs;
