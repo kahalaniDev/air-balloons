32dd146bc1371dd7d568fb4aa4878fc2533a88a5
daniel kahalani <daniel.kahalani@devalore.com>
2022-03-28 17:05:35 +0300

    remove imports from LoginForm.test

--- client/src/features/account/components/LoginForm.test.tsx
+++ client/src/features/account/components/LoginForm.test.tsx
@@ -1,14 +1,10 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { act } from "react-dom/test-utils";
-
 import { MemoryRouter } from "react-router";
 import ApolloProvider from "../../../components/apollo/ApolloProvider";
 import LoginForm from "./LoginForm";
 import { Provider as ReduxProvider } from "react-redux";
 import { createStore } from "../../../infrastructure/redux/store";
-import Routes from "../../../components/navigation/Routes";
-// import { validateUsername, validatePassword } from "../utils/validators";
 
 describe("LoginForm", () => {
   test("validate empty inputs and showing error", async () => {
@@ -58,12 +54,14 @@ describe("LoginForm", () => {
     userEvent.type(screen.getByLabelText(/Username/i), "daniel");
     userEvent.type(screen.getByLabelText(/Password/i), "!1234567");
     expect(screen.getByRole("button", { name: /SIGN IN/i })).toBeEnabled();
-    await userEvent.click(screen.getByRole("button", { name: /SIGN IN/i }));
+    userEvent.click(screen.getByRole("button", { name: /SIGN IN/i }));
     // eslint-disable-next-line testing-library/prefer-find-by
+    await waitFor(() => expect(screen.getByRole("circle")).toBeInTheDocument());
     await waitFor(() =>
       expect(
         screen.queryByText(/incorrect username or password/i)
       ).not.toBeInTheDocument()
     );
+    screen!.debug();
   });
 });
